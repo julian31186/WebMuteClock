2080be6843943e54dd0989b94dfd87ff2ca88e99
Julian Elmasry <julian31186@gmail.com>
2022-03-24 12:14:37 -0400

    year change

--- web-mute-clock/src/App.js
+++ web-mute-clock/src/App.js
@@ -11,7 +11,7 @@ function App() {
   let interval;
 
   const startTimer=()=> {
-    const countDownDate = new Date("May 8, 2023").getTime();
+    const countDownDate = new Date("May 8, 2022").getTime();
     interval=setInterval(()=> {
       const now = new Date().getTime();
       const distance = countDownDate - now;
